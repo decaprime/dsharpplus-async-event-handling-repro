cb54d91741e5765733c1e81aafbc425ce367321b
deca <62450933+decaprime@users.noreply.github.com>
2021-05-05 19:43:14 -0700

    Initial commit of repro for report

---
+++ Program.cs
@@ -0,0 +1,151 @@
+// THIS FILE IS A PART OF EMZI0767'S BOT EXAMPLES
+//
+// --------
+// 
+// Copyright 2019 Emzi0767
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// --------
+//
+// This is a basic example. It shows how to set up a project and connect to 
+// Discord, as well as perform some simple tasks.
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DSPlus.Examples
+{
+	public class Program
+	{
+		public readonly EventId BotEventId = new EventId(42, "Bot-Ex01");
+
+		public DiscordClient Client { get; set; }
+
+		public static void Main(string[] args)
+		{
+			// since we cannot make the entry method asynchronous,
+			// let's pass the execution to asynchronous code
+			var prog = new Program();
+			prog.RunBotAsync().GetAwaiter().GetResult();
+		}
+
+		public async Task RunBotAsync()
+		{
+			// first, let's load our configuration file
+			var json = "";
+			using (var fs = File.OpenRead("config.json"))
+			using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+				json = await sr.ReadToEndAsync();
+
+			// next, let's load the values from that file
+			// to our client's configuration
+			var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+			var cfg = new DiscordConfiguration
+			{
+				Token = cfgjson.Token,
+				TokenType = TokenType.Bot,
+
+				AutoReconnect = true,
+				MinimumLogLevel = LogLevel.Debug
+			};
+
+			// then we want to instantiate our client
+			this.Client = new DiscordClient(cfg);
+
+			// next, let's hook some events, so we know
+			// what's going on
+			this.Client.Ready += this.Client_Ready;
+			this.Client.GuildAvailable += this.Client_GuildAvailable;
+			this.Client.ClientErrored += this.Client_ClientError;
+			this.Client.MessageReactionAdded += this.Client_MessageReactionAdded;
+			this.Client.MessageReactionRemoved += this.Client_MessageReactionRemoved;
+
+			AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+
+			// finally, let's connect and log in
+			await this.Client.ConnectAsync();
+
+			// and this is to prevent premature quitting
+			await Task.Delay(-1);
+		}
+
+		private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+		{
+			this.Client.Logger.LogError(BotEventId, e.Exception, " [FirstChance]");
+		}
+
+		private async Task Client_MessageReactionAdded(DiscordClient sender, MessageReactionAddEventArgs e)
+		{
+			await Task.Delay(300);
+			throw new System.NotImplementedException(">>>This is not shown in Client_ClientErrored<<<");
+		}
+
+		private Task Client_MessageReactionRemoved(DiscordClient sender, MessageReactionRemoveEventArgs e)
+		{
+			throw new System.NotImplementedException(">>>This shows in Client_ClientErrored<<<");
+		}
+
+		private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+		{
+			// let's log the fact that this event occured
+			sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+			// since this method is not async, let's return
+			// a completed task, so that no additional work
+			// is done
+			return Task.CompletedTask;
+		}
+
+		private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+		{
+			// let's log the name of the guild that was just
+			// sent to our client
+			sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+			// since this method is not async, let's return
+			// a completed task, so that no additional work
+			// is done
+			return Task.CompletedTask;
+		}
+
+		private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+		{
+			// let's log the details of the error that just 
+			// occured in our client
+			sender.Logger.LogCritical(BotEventId, e.Exception, "Exception occured");
+
+			// since this method is not async, let's return
+			// a completed task, so that no additional work
+			// is done
+			return Task.CompletedTask;
+		}
+	}
+
+	// this structure will hold data from config.json
+	public struct ConfigJson
+	{
+		[JsonProperty("token")]
+		public string Token { get; private set; }
+
+		[JsonProperty("prefix")]
+		public string CommandPrefix { get; private set; }
+	}
+}
